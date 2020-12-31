dc38fdaac81bd7608337b5a0c9e874768d00fec9
Ali Bubere <alibubere9@gmail.com>
2020-12-31 19:28:47 +0530

    done cart

--- lib/provider_class/provider_class.dart
+++ lib/provider_class/provider_class.dart
@@ -5,33 +5,52 @@ import 'package:furniture_app/models/product.dart';
 class Cart extends ChangeNotifier {
   List<CartModel> _productItemList = [];
   int quantity = 1;
-  //List<Product> _duplicatesList = [];
 
-  addQuantity(CartModel cartModel) {
-    cartModel.quantity++;
-  }
-
-  removeQuantity() {}
 
   addProductItem(Product product) {
-    if (_productItemList.contains(product)) {
-      quantity++;
+    if (_productItemList.isNotEmpty) {
+      CartModel cartItem = _productItemList
+          ?.firstWhere((element) => element.product.id == product.id);
+      if (cartItem == null) {
+        _addNewProduct(product);
+      } else {
+        _incrementQuantity(cartItem);
+      }
+    } else {
+      _addNewProduct(product);
     }
-    CartModel productItem = CartModel(product: product, quantity: quantity);
-    // title: product.title,
-    // price: product.price,
-    // image: product.image,
-    // id: product.id,
-    // description: product.description,
-    // category: product.category);
-
-    // List<Product> duplicList =
-    //     _productItemList.where((element) => element.id == product.id).toList();
-    // if (duplicList.isEmpty) {
+    // if (_productItemList.contains(product)) {
+    //   quantity++;
     // }
-    _productItemList.add(productItem);
+    // CartModel productItem = CartModel(product: product, quantity: quantity);
+    // // title: product.title,
+    // // price: product.price,
+    // // image: product.image,
+    // // id: product.id,
+    // // description: product.description,
+    // // category: product.category);
+
+    // // List<Product> duplicList =
+    // //     _productItemList.where((element) => element.id == product.id).toList();
+    // // if (duplicList.isEmpty) {
+    // // }
+    // _productItemList.add(productItem);
 
     notifyListeners();
+  }
+
+  void _incrementQuantity(CartModel cartItem) {
+    int _index = _productItemList.indexOf(cartItem);
+    _productItemList[_index].quantity++;
+  }
+
+  void _decrementQuantity(CartModel cartItem) {
+    int _index = _productItemList.indexOf(cartItem);
+    _productItemList[_index].quantity--;
+  }
+
+  void _addNewProduct(Product product) {
+    _productItemList.add(CartModel(product: product, quantity: 1));
   }
 
   removeProductItem(CartModel product) {
